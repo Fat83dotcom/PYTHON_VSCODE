f5a18e9b6f76e832eed8b29af7e3e65968fcc3fb
fernando <fernando.quimic@gmail.com>
2022-05-08 21:28:22 -0300

    ajuste

--- Numeros_primos_intervalo.py
+++ Numeros_primos_intervalo.py
@@ -1,16 +1,16 @@
 def primos(ini, fim):
     div = 0
-    primos = list()
+    primo = list()
     for i in range(ini, fim):
         for j in range(1, i + 1):
             if i % j == 0:
                 div += 1
         if div == 2:
-            primos.append(i)
+            primo.append(i)
             div = 0
         else:
             div = 0
-    print(primos)
+    print(primo)
 
 
-primos(100, 300)
+primos(100, 10000)
