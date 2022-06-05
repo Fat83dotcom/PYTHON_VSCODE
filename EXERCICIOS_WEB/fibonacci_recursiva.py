c684a1ce52674c025286ba3979b23771c395c511
fernando <fernando.quimic@gmail.com>
2022-06-05 10:52:13 -0300

    Meu algoritmo Fibonacci Recursivo

---
+++ EXERCICIOS_WEB/fibonacci_recursiva.py
@@ -0,0 +1,13 @@
+def fibo(cont, b=0, a=1, retorno=[]): 
+    if b == 0:
+        cont -= 2 
+        retorno.append(b)
+        print(b)
+    retorno.append(a)
+    print(a)
+    if cont == 0:
+        return retorno
+    else:
+        return fibo(cont-1, b=a, a=a+b)
+
+print(fibo(10))
